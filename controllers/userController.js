c74db11aeda768edc11646323cf0742eb0b6f239
Ashik k <ashikash796@gmail.com>
2024-05-21 23:37:38 +0530

    feat(Auth):Login Functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,8 +37,43 @@ const userRegister = async (req, res) => {
   }
 };
 
+const userLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const exists = await userModel.findOne({ email: email });
+    if (exists) {
+      const access = await bcrypt.compare(password.toString(), exists.password);
+      if (access) {
+        const token = jwt.sign({ userId: exists._id }, process.env.JWT_SECRET, {
+          expiresIn: 6000000,
+        });
+        return res.status(200).json({
+          user: exists,
+          token: token,
+          message: "Login Successfull",
+          status: true,
+        });
+      } else {
+        return res
+          .status(404)
+          .json({ message: "Email or Password is wrong", status: false });
+      }
+    } else {
+      return res
+        .status(201)
+        .json({ message: "This Email is Not Registered", status: false });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: true, message: error.message });
+  }
+};
+
+
+
 
 
 module.exports = {
   userRegister,
+  userLogin,
 };
